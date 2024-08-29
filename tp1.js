f2e8a110afd40c679d0828223835e88fb8b89336
Sokol Tobias <167479288+Sokol-tobias@users.noreply.github.com>
2024-08-29 18:19:38 -0300

    Add files via upload

---
+++ tp1.js
@@ -0,0 +1,42 @@
+/*
+Nombre: Sokol Tobias Ezequiel
+ legajo:120375/9
+ Comision 5
+ */
+ let rotacion = false;
+function preload(){
+pintura=loadImage('data/pintura.jpg');
+let colorActual;
+let colorInicial = [93, 185, 185];
+let rotacion = false;
+}
+function setup() {
+  createCanvas(800, 400);
+  background(255);
+  noStroke();
+  colorActual = color(colorInicial[0], colorInicial[1], colorInicial[2]);
+}
+
+function draw() {
+  fill(93, 185, 185);
+  rect(width / 2, 0, width / 2, height);
+  
+  if (rotacion === false) {
+    circulos();
+  } else {
+    circulos1();
+  }
+  
+  image(pintura, 0, 0, width / 2, height);
+  
+  fill(255);
+  textSize(15);
+  text("presiona 'a' o 'd' para aumentar o disminuir la velocidad.\nPresiona 'r' para cambiar a un color al azar\npresiona barra espaciadora para reiniciar", 10, 10);
+}
+
+function mouseClicked() {
+  if (mouseX > 400 && mouseX < 800 && mouseY > 0 && mouseY < 400) {
+    rotacion = !rotacion;
+    console.log(rotacion);
+  }
+}
