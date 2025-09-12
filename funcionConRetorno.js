a2f79b5c6e5cc1691363867f169becd5668b09bc
Sokol Tobias <167479288+Sokol-tobias@users.noreply.github.com>
2025-09-12 01:53:50 -0300

    Add files via upload

---
+++ funcionConRetorno.js
@@ -0,0 +1,43 @@
+let colorInicial = [167, 178, 48];
+let colorActual;
+let valoresAleatorios;
+
+function setup() {
+  colorActual = color(colorInicial[0], colorInicial[1], colorInicial[2]);
+  valoresAleatorios = obtenerValoresAleatorios();
+  console.log("Valores aleatorios generados: " + valoresAleatorios[0] + ", " + valoresAleatorios[1] + ", " + valoresAleatorios[2]);
+}
+
+function obtenerValoresAleatorios() {
+  let valores = [];
+  for (let i = 0; i < 3; i++) {
+    valores[i] = int(random(256));
+  }
+  return valores;
+}
+
+function keyPressed() {
+  // Botón de color aleatorio
+  if (key === 'r' || key === 'R') {
+    valoresAleatorios = obtenerValoresAleatorios();
+    colorActual = color(valoresAleatorios[0], valoresAleatorios[1], valoresAleatorios[2]);
+    console.log("Color actualizado: (" + valoresAleatorios[0] + ", " + valoresAleatorios[1] + ", " + valoresAleatorios[2] + ")");
+  }
+
+  // Botón para aumentar y disminuir la velocidad de giro
+  if (key === 'a') { // disminuye
+    spdRot -= 0.05;
+    console.log(spdRot);
+  }
+  if (key === 'd') { // aumenta
+    spdRot += 0.05;
+    console.log(spdRot);
+  }
+
+  // Botón para reiniciar los valores
+  if (key === ' ') {
+    rotacion = false;
+    colorActual = color(colorInicial[0], colorInicial[1], colorInicial[2]);
+    spdRot = 0.05;
+  }
+}
