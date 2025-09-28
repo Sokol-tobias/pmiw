dde9e2f6213ce6f8253b7e9843b360425a1370b8
Sokol Tobias <tbssokol.55@gmail.com>
2025-09-28 18:37:31 -0300

    agregacion de pagina de funciones

---
+++ tpfinalparte1/Funciones.js
@@ -0,0 +1,5 @@
+function textos(){
+  if (pantalla = 0){
+
+  }
+}
--- tpfinalparte1/tpfinalparte1.js
+++ tpfinalparte1/tpfinalparte1.js
@@ -1,11 +1,17 @@
 let fondoX = 640, fondoY = 480;
+let txt;
+let pantalla;
+function preload(){
+txt = loadStrings ('texto.txt');
+}
 
 function setup() {
 createCanvas (fondoX, fondoY);
+console.log(txt);
 }
 
 
 function draw() {
 background(125);
-cursor('imagenes/shurikenCursor.png');
+cursor('shurikenCursor.png');
 }
