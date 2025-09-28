3ad096f618e9f97318515b04dc1f85a0bc56db9a
Sokol Tobias <tbssokol.55@gmail.com>
2025-09-28 16:52:30 -0300

    cambio de cursor

--- tpfinalparte1/tpfinalparte1.js
+++ tpfinalparte1/tpfinalparte1.js
@@ -1,8 +1,11 @@
-function setup() {
+let fondoX = 640, fondoY = 480;
 
+function setup() {
+createCanvas (fondoX, fondoY);
 }
 
 
 function draw() {
-
+background(125);
+cursor('imagenes/shurikenCursor.png');
 }
