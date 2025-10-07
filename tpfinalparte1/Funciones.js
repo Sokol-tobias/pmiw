68982239b257dfa54005e6dea615f14eaa029489
Sokol Tobias <tbssokol.55@gmail.com>
2025-10-07 20:03:33 -0300

    agregue video y cositas

--- tpfinalparte1/Funciones.js
+++ tpfinalparte1/Funciones.js
@@ -3,3 +3,13 @@ function textos(){
 
   }
 }
+
+function videos() {
+  if(mouseIsPressed && estado == 'intro'){  
+  video.play();
+  image(video, 0, 0, width, height);
+  if(estado == 'juego'){
+    pantallas [0];
+  }
+  }
+}
--- tpfinalparte1/tpfinalparte1.js
+++ tpfinalparte1/tpfinalparte1.js
@@ -1,10 +1,12 @@
 let fondoX = 640, fondoY = 480;
 let txt;
 let pantallas = [];
+let video , estado = 'intro';
+
 function preload(){
 txt = loadStrings ('texto.txt');
-loadVideo();
-for(let i=0; i<=pantallas.length ; i++){
+
+for(let i=0; i<=18 ; i++){
     let nombre = "pantalla"+i+".jpeg";
     pantallas.push(loadImage("imagenes/" + nombre));
   }
@@ -12,11 +14,18 @@ for(let i=0; i<=pantallas.length ; i++){
 
 function setup() {
 createCanvas (fondoX, fondoY);
+video = createVideo('videos/intro.mp4');
+video.hide();
+video.onended(() => {
+  video.hide();
+  estado = 'juego';
+});
 console.log(pantallas.length);
 }
 
 
 function draw() {
 background(125);
+videos();
 cursor('shurikenCursor.png');
 }
